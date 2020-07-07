8c6d959cb6fca947535c7a3ab3fc46de831734bd
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2020-07-07 11:14:06 +0200

    add /01-Ruby/01-Programming-basics/Lecture

---
+++ 01-Ruby/01-Programming-basics/Lecture/test.rb
@@ -0,0 +1,78 @@
+require 'date'
+
+# name1 = "toto"
+# name2 = "tata"
+# name3 = "tutu"
+# name4 = "titi"
+
+
+# puts "hello #{name1}"
+# puts "hello #{name2}"
+# puts "hello #{name3}"
+# puts "hello #{name4}"
+
+
+# def hello(name)
+#   xxx = "hello #{name.upcase}"
+#   return xxx
+# end
+
+
+
+
+# name1 = "toto"
+# name2 = "tata"
+# name3 = "tutu"
+
+# hello_sentence = hello(name1)
+
+# puts hello_sentence
+
+# puts hello(name1)
+# puts hello(name2)
+# puts hello(name3)
+
+
+# def max(x, y)
+#   if x > y
+#     return x
+#   else
+#     return y
+#   end
+# end
+
+# first_number = 6
+# second_number = 9
+
+# largest = max(first_number, second_number)
+
+# puts largest
+
+
+# def tomorrow
+#   tomorrow_date = Date.today + 1
+#   return tomorrow_date.strftime("%B %d")
+# end
+
+
+
+# puts tomorrow
+
+
+# def add(x, y)
+#   x + y
+# end
+
+
+# puts add(3, 5)
+
+
+# def ma fonction
+
+# end
+
+# puts ma "ghj"
+
+
+
+
