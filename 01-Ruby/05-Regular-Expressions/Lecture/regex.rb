bad06624b1d3a2df4b00f9608ee2dd7b7368142f
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2020-07-13 10:29:33 +0200

    add /01-Ruby/05-Regular-Expressions/Lecture

---
+++ 01-Ruby/05-Regular-Expressions/Lecture/regex.rb
@@ -0,0 +1,21 @@
+require 'awesome_print'
+
+# if phone_number.match?(/^\+\d{2}\s\d{2}\s\d{4}\s\d{4}$/)
+#   puts "This is a valid UK international phone number"
+# else
+#   puts "It's not valid!"
+# end
+
+
+# r = /(?<firstname>\w+)\s(?<lastname>\w+)/
+
+# full_name = "!!! toto DUPONT !!!"
+
+# m = full_name.match(r)
+
+# ap m
+# ap m[:firstname]
+# ap m[:lastname]
+
+
+puts "Let's play tic tac toe".scan(/\bt..\b/)
