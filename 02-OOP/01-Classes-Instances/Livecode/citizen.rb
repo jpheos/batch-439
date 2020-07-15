7c3c65ca5aefe07789147b1c9c079b069ebc7d7e
Alexandre Bouvier <alexandre.b2506@gmail.com>
2020-07-15 18:05:24 +0200

    fix bug from previous commit

--- 02-OOP/01-Classes-Instances/Livecode/citizen.rb
+++ 02-OOP/01-Classes-Instances/Livecode/citizen.rb
@@ -2,11 +2,14 @@ class Citizen
   def initialize(first_name, last_name, age)
     @first_name = first_name
     @last_name  = last_name
+    @age        = age
   end
+
   def can_vote?
     # @age >= 18 ? true : false
     @age >= 18
   end
+
   def full_name
     "#{@first_name} #{@last_name}"
   end
